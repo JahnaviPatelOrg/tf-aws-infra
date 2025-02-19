ec6e5706df272b081ff2a9af1404c752936a399b
Jahnavi Bhagirath Patel <patel.jahnavi@northeastern.edu>
2025-02-18 21:28:50 -0500

    AWS VPC using Terraform

---
+++ main.tf
@@ -0,0 +1,78 @@
+
+# creating vpc
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+# creating 3 public subnets in different availability zones
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.public_subnets, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+
+  tags = {
+    Name = "${var.vpc_name}-public-subnet-${count.index + 1}"
+  }
+}
+
+# creating 3 private subnets in different availability zones
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnets, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+  tags = {
+    Name = "${var.vpc_name}-private-subnet-${count.index + 1}"
+  }
+}
+
+# Set up Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
+
+# Set up public route table
+resource "aws_route_table" "second_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-public-route-table"
+  }
+}
+
+# Attach all public subnets created to the route table.
+resource "aws_route_table_association" "public_subnet_asso" {
+  count          = length(var.public_subnets)
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.second_rt.id
+}
+
+# create a private route table (no internet access)
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-private-route-table"
+  }
+}
+
+# Attach all private subnets created to the route table.
+resource "aws_route_table_association" "private_subnet_asso" {
+  count          = length(var.private_subnets)
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = aws_route_table.private.id
+}
