2b42fb361b1a66b9a9ab4ab91309aedbc1c5520e
Jahnavi Bhagirath Patel <patel.jahnavi@northeastern.edu>
2025-02-18 22:23:40 -0500

    testing action fail

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "aws_route_table" "second_rt" {
 
 # Attach all public subnets created to the route table.
 resource "aws_route_table_association" "public_subnet_asso" {
-  count          = length(var.public_subnets)
+  count= length(var.public_subnets)
   subnet_id      = element(aws_subnet.public.*.id, count.index)
   route_table_id = aws_route_table.second_rt.id
 }
