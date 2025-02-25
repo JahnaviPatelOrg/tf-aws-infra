6abc8ac74198d83f7b9f038e9f19b399b4f849a6
Jahnavi Bhagirath Patel <patel.jahnavi@northeastern.edu>
2025-02-25 14:39:58 -0500

    demo 2

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 # creating vpc
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+  cidr_block = varr.vpc_cidr
 
   tags = {
     Name = var.vpc_name
@@ -9,7 +9,7 @@ resource "aws_vpc" "main" {
 }
 
 # creating 3 public subnets in different availability zones
-  resource "aws_subnet" "public" {
+resource "aws_subnet" "public" {
   count             = length(var.public_subnets)
   vpc_id            = aws_vpc.main.id
   cidr_block        = element(var.public_subnets, count.index)
